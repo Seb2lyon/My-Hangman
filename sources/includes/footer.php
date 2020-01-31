9d9e133713ed03fdae89962a6d4f5a2384e927f1
Seb2lyon <seb2lyon.info@free.fr>
2020-01-31 18:33:10 +0100

    Update footer.php
    Update version number

--- sources/includes/footer.php
+++ sources/includes/footer.php
@@ -1,5 +1,5 @@
 <footer>
 
-	<p>Seb2lyon - version 1.1 - 06/03/2017<a href="http://seb2lyon.info.free.fr">Accueil</a><a href="https://github.com/Seb2lyon/My-Hangman">Sources (GitHub)</a><a href="https://www.gnu.org/licenses/gpl.txt">Licence GPL v.3</a></p>
+	<p>Seb2lyon - version 1.2 - 01/02/2020<a href="http://seb2lyon.info.free.fr">Accueil</a><a href="https://github.com/Seb2lyon/My-Hangman">Sources (GitHub)</a><a href="https://www.gnu.org/licenses/gpl.txt">Licence GPL v.3</a></p>
 
 </footer>
